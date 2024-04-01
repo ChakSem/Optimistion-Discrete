b4ad06f844de893631ed4018ffb1177e2057ae4f
Sheerven <ayordletamer@gmail.com>
2024-04-01 19:36:31 +0200

    Avancement

--- Code Opti Tournees Touristiques + Donnees-20240315/Project1/Project1/Heuristique_v1.cpp
+++ Code Opti Tournees Touristiques + Donnees-20240315/Project1/Project1/Heuristique_v1.cpp
@@ -1,14 +1,52 @@
 #include "Heuristique_v1.h"
 
-#define POI_SCORE_INITIAL 1
-#define POI_TEMPS_OUVERT 1
+#define POI_SCORE_INITIAL 1.0
+#define POI_TEMPS_OUVERT 1.0
 
-#define HOTEL_SCORE_POI 1
-#define HOTEL_DISTANCE_AU_POI 1
+#define HOTEL_SCORE_POI 1.0
+#define HOTEL_DISTANCE_AU_POI 1.0
+
+void Heuristique_v1::Solution1() 
+{
+    // A tester
+    for (int i : pi_POI_Coherents) {
+        CalculScorePOI(i);
+    }
+
+    H = Instance->get_Id_Hotel_depart();
+
+    for (i_Jour = 0; i_Jour < Instance->get_Nombre_Jour(); i_Jour++) {
+        i_Duree_Journee_En_Cours = Instance->get_POI_Duree_Max_Voyage(i_Jour);
+
+        IdentifierHotelsDemiRayon(H);
+
+        int i_Meilleur_Hotel = -1;
+        double i_Score_Meilleur_Hotel = -1;
+        vector<int> pi_Rayon_Meilleur_Hotel;
+        for (int i_Hotel_Possible : pi_Rayon_Hotels) {
+
+            vector<int> pi_Rayon = IdentifierPOIRayonHotel(i_Hotel_Possible);
+
+            double i_Score = CalculScoreHotel(i_Hotel_Possible, pi_Rayon);
+
+            if (i_Score > i_Score_Meilleur_Hotel) {
+                i_Meilleur_Hotel = i_Hotel_Possible;
+                i_Score_Meilleur_Hotel = i_Score;
+                pi_Rayon_Meilleur_Hotel = pi_Rayon;
+            }
+        }
+
+        CalculMeilleurJournee(H, i_Meilleur_Hotel);
+        H = i_Meilleur_Hotel;
+
+        i_Duree_Max -= i_Duree_Journee_En_Cours;
+    }
+}
 
 void Heuristique_v1::CalculScorePOI(int i_POI_Param)
 {
-    int i_Score_Total = POI_SCORE_INITIAL * Instance->get_POI_Score(i_POI_Param) + POI_TEMPS_OUVERT * (Instance->get_POI_Heure_fermeture(i_POI_Param) - Instance->get_POI_Heure_ouverture(i_POI_Param));
+    // A tester
+    double i_Score_Total = POI_SCORE_INITIAL * Instance->get_POI_Score(i_POI_Param) + (Instance->get_POI_Heure_fermeture(i_POI_Param) - Instance->get_POI_Heure_ouverture(i_POI_Param) / POI_TEMPS_OUVERT);
 
     pi_Score_POI[i_POI_Param] = i_Score_Total;
 }
@@ -16,66 +54,77 @@ void Heuristique_v1::CalculScorePOI(int i_POI_Param)
 /* Permet d'identifier les hotels sur un demi rayon de */
 void Heuristique_v1::IdentifierHotelsDemiRayon(int i_Hotel_Depart)
 {
-    // liste id hotel
-
-    /*On recupere les hotels qui qui sont dans realisables dans un intervalle d'une journée (demi rayon)*/
-
-    for (int i_Hotel = 0; i_Hotel < Instance->get_Nombre_Hotel(); i_Hotel++)
+    // A tester
+    int i_id_Hotels_Coherents = 0;
+    while (i_id_Hotels_Coherents < pi_Hotels_Coherents.size())
     {
-        if (Instance->get_distance_Hotel_Hotel(i_Hotel_Depart, i_Hotel) > Instance->get_POI_Duree_Max_Voyage())
-        {
-            pi_Id_Hotels.push_back(i_Hotel);
+        int i_Hotel_Coherent = pi_Hotels_Coherents[i_id_Hotels_Coherents];
+
+        if (Instance->get_distance_Hotel_Hotel(i_Hotel_Coherent, Instance->get_Id_Hotel_Arrivee()) > i_Duree_Max - i_Duree_Journee_En_Cours) {
+            pi_Hotels_Coherents.erase(pi_Hotels_Coherents.begin() + i_id_Hotels_Coherents);
+        }
+        else {
+            if (Instance->get_distance_Hotel_Hotel(i_Hotel_Depart, i_Hotel_Coherent) > Instance->get_POI_Duree_Max_Voyage(i_Jour))
+            {
+                pi_Rayon_Hotels.push_back(i_Hotel_Coherent);
+            }
+
+            i_id_Hotels_Coherents++;
         }
     }
 }
 
-vector<int> Heuristique_v1::IdentifierPOIRayonHotel(int i_Hotel_Param, int i_Id_Journee)
+vector<int> Heuristique_v1::IdentifierPOIRayonHotel(int i_Hotel_Param)
 {
-    // liste POI
-
+    // A tester
     vector<int> pi_POI_DANS_LE_RAYON = {};
 
-    for (int i_id_POI = 0; i_id_POI < Instance->get_Nombre_POI(); i_id_POI++)
+    for (int i_POI_Coherent : pi_POI_Coherents)
     {
-        if (pb_Id_POI_Pour_Score[i_id_POI] == POI_NON_OBSOLETE &&
-            Instance->get_distance_Hotel_POI(i_Hotel_Param, i_id_POI) > Instance->get_POI_Duree_Max_Voyage(i_Id_Journee))
+        if (Instance->get_distance_Hotel_POI(i_Hotel_Param, i_POI_Coherent) + 
+            Instance->get_distance_Hotel_POI(H, i_POI_Coherent) < i_Duree_Journee_En_Cours)
         {
-
-            pi_POI_DANS_LE_RAYON.push_back(i_id_POI);
+            pi_POI_DANS_LE_RAYON.push_back(i_POI_Coherent);
         }
     }
 
     return pi_POI_DANS_LE_RAYON;
 }
 
-void Heuristique_v1::CalculScoreHotel(int i_Hotel_Param, vector<int> pi_POI_Dans_Le_Rayon)
+double Heuristique_v1::CalculScoreHotel(int i_Hotel_Param, vector<int> pi_POI_Dans_Le_Rayon)
 {
-    // liste POI calculer precedment
-    // on stocke la dessus pi_Id_Hotels
-
-    int i_Score_Total = 0;
+    // A tester
+    double i_Score_Total = 0;
 
     for (int Id_POI : pi_POI_Dans_Le_Rayon)
     {
-        //Pour obtenir le score total 
-        i_Score_Total += pi_Score_POI[Id_POI] * HOTEL_SCORE_POI;
-    }
-    // pi_Id_Hotels[i_Hotel_Param] = i_Score_Total;
-}
+        double i_Score = pi_Score_POI[Id_POI] * HOTEL_SCORE_POI;
 
-int Heuristique_v1::SelectionnerMeilleurHotel()
-{
-    int i_Meilleur_Hotel = 0;
-    int i_Score_Meilleur_Hotel = -1;
+        i_Score = HOTEL_DISTANCE_AU_POI * (i_Score / Instance->get_distance_Hotel_POI(H, Id_POI));
 
-    for (int i_Hotel = 0; i_Hotel < Instance->get_Nombre_Hotel(); i_Hotel++)
-    {
-        if (pi_Id_Hotels != HOTEL_OBSOLETE) {
-            // TODO
-        }
+        i_Score_Total += i_Score;
     }
+    
+    return i_Score_Total;
 }
 
 void Heuristique_v1::CalculMeilleurJournee(int i_Hotel_Depart, int i_Hotel_Arrive)
 {
+    // TODO
+}
+
+void Heuristique_v1::Initialisation()
+{
+    for (int i = 0; i <= Instance->get_Nombre_POI(); ++i) {
+        pi_POI_Coherents.push_back(i);
+    }
+
+    for (int i = 0; i <= Instance->get_Nombre_Hotel(); ++i) {
+        pi_Hotels_Coherents.push_back(i);
+    }
+
+    i_Duree_Max = Instance->get_POI_Duree_Max_Voyage(0);
+    for (int i = 1; i < Instance->get_Nombre_Jour(); ++i) {
+        i_Duree_Max += Instance->get_POI_Duree_Max_Voyage(i);
+    }
 }
--- Code Opti Tournees Touristiques + Donnees-20240315/Project1/Project1/Heuristique_v1.h
+++ Code Opti Tournees Touristiques + Donnees-20240315/Project1/Project1/Heuristique_v1.h
@@ -7,30 +7,78 @@
 #define POI_OBSOLETE false
 #define POI_NON_OBSOLETE true
 
+/**
+ * Determination d'une solution :
+ * 
+ * Etape 1 : Calculer un score pour chaque POI / Score :    + score du POI
+ *                                                          / temps ouvet sur une journée
+ * 
+ * On définit H <= Hotel de départ
+ * 
+ * Repeter  
+ *    |     (Eliminer les Hotels qui sont à une distance supérieure au nombre de jours restants + qui font revenir en arriere (absice negative sur la projection de H à l'Hotel d'arrivé))
+ *    |     Etape 2 : Identifier hotel atteignable en 1 journée à partir de l'hotel H
+ *    |
+ *    |     (Eliminer les POI qui sont à une distance supérieure au nombre de jours restants)
+ *    |     Etape 3 : Pour chacun de ses hotels, calculer et associer un score / Score :    + score de chaque POI à moins de 1 journée (H => POI => Hotel d'arrive)
+ *    |                                                                                     => diviser par la distance de l'Hotel au POI
+ *    |                                                                                          
+ *    |     Etape 4 : Hotel H' <= Hotel avec le score le plus élevé
+ *    |     
+ *    |     Etape 5 : Calcul du chemin qui maximise le score de H à H'
+ *    |     (Eliminer les POI selectionnés)
+ *    |     
+ *    |     Etape 6 : H <= H'
+ * Jusqu'à nb de jour restant = 0 (à la dernière iteration H' est d'office l'hotel d'arrivé
+ * 
+ * ============================================================================================================================================================
+ * Explorer de nouvelles solutions :
+ * 
+ * Solution 2 ) Refaire l'algo du 1er Hotel selectionné (après l'hotel de départ) à l'hotel d'arrivé, puis trouver le calcul maximal de l'hotel de depart 
+ *              à cet hotel (sans les POI deja selectionné)
+ * 
+ * Solution 3 ) Recommencer à partir du 2eme hotel, puis refaire l'algo de l'hotel de départ au 2e hotel (sans les POI selctionnée lors du 1er algo)
+ * 
+ * ... 
+ * (Attention à choisir le n-ième hotel à partir de la meilleure solution)
+ * 
+ * 
+ * Solution n+1 )   Refaire tout ca dans l'autre sens
+ * 
+ * ...
+ * 
+ */
+
+
 class Heuristique_v1
 {
 private:
+    int i_Jour;
+    int i_Duree_Max;
+    int i_Duree_Journee_En_Cours;
+    int H;
+
     Instance *Instance;
-    vector<int> pi_Score_POI;          // Liste de taille n, stockant le score calculee du POI
-    vector<int> pi_Id_Hotels;          /* Liste de taille m (nb d'Hotels) :  -1 initialement
-                                                                             -2 si l'Hotel est obsolete
-                                                                             son score sinon*/
-    vector<bool> pb_Id_POI_Pour_Score; /* Liste de taille n (nb de POI), puis chaque POI est elimines chaques fois qu'il devient obsolete
-                                                                              - true : non_obsolete
-                                                                              - false : obsolete*/
+    vector<double> pi_Score_POI;          // Liste de taille n, stockant le score calculee du POI
+    vector<int> pi_Rayon_Hotels;          /* Liste contenant les hotels dans le rayon de l'hotel choisit en debut d'iteration*/
 
     vector<int> pi_Solution; // Liste des id dans l'ordre des solution
 
+    vector<int> pi_POI_Coherents;
+    vector<int> pi_Hotels_Coherents;
+
 public:
+    void Solution1();
+
     void CalculScorePOI(int iPOI_Param);
     /* Permet d'identifier les hotels sur un demi rayon de */
     void IdentifierHotelsDemiRayon(int i_Hotel_Param);
     /* Permet d'intialiser les scores des hotels sur un demi rayon de */
-    vector<int> IdentifierPOIRayonHotel(int i_Hotel_Param, int i_Id_Journee);
+    vector<int> IdentifierPOIRayonHotel(int i_Hotel_Param);
     /* Permet de calculer le score d'un hotel */
-    void CalculScoreHotel(int i_Hotel_Param, vector<int> pi_POI_Dans_Le_Rayon);
-    /* Permet de determiner le meilleur hotel (celui qui a le meilleur score dans son rayon) */
-    int SelectionnerMeilleurHotel();
+    double CalculScoreHotel(int i_Hotel_Param, vector<int> pi_POI_Dans_Le_Rayon);
     /* Permet de calculer la meilleure journée entre deux hotels*/
     void CalculMeilleurJournee(int i_Hotel_Depart, int i_Hotel_Arrive);
+
+    void Initialisation();
 };
